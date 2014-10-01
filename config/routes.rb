2ade1fca8c6cb5f6c8150e1c229f2ee189b14285
Michael Deng <michaeldeng18@gmail.com>
2014-10-01 09:30:35 -0700

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Localwise::Application.routes.draw do
-  get "static_pages/home"
+  root  'static_pages#home'
+  match '/about', to: 'static_pages#about', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
